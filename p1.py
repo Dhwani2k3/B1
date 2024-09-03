76f8ea276a7759a91a993c6742adeba13e392173
DHWANI GUPTA <Dhwani.15985@stu.upes.ac.in>
2024-09-03 15:24:45 +0530

    Completing my python assignment

---
+++ p1.py
@@ -0,0 +1,7 @@
+print("Name : Dhwani Gupta")
+print("Course : MCA - Master of Computer Application")
+print("sap id :590015985")
+import platform
+print("Python Version : ",platform.python_version())
+
+      
